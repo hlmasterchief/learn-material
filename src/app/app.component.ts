dc6a4220fc0860e1ceb14b3b178fc35af4888bf4
Long Hoang <longh1@bidv.com.vn>
2020-11-14 12:23:10 +0700

    day03

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  board1 = {
+export class AppComponent implements OnInit {
+  board1: Board = {
     title: 'Todo',
     items: [
       'Learn Angular Material',
@@ -15,13 +18,13 @@ export class AppComponent {
       'Code Final Project'
     ]
   };
-  board2 = {
+  board2: Board = {
     title: 'Doing',
     items: [
       'Learn Angular'
     ]
   };
-  board3 = {
+  board3: Board = {
     title: 'Done',
     items: [
       'Learn HTML basic',
@@ -33,7 +36,16 @@ export class AppComponent {
       'Learn Responsive Web Design'
     ]
   };
-  boards = [this.board1, this.board2, this.board3];
+  boards: Board[];
+
+  constructor(private dialog: MatDialog, private snackBar: MatSnackBar) {}
+
+  ngOnInit(): void {
+    if (!this.getBoards() || !this.getBoards().length) {
+      this.setBoards([this.board1, this.board2, this.board3]);
+    }
+    this.boards = this.getBoards();
+  }
 
   drop(event: CdkDragDrop<string[]>): void {
     if (event.previousContainer === event.container) {
@@ -44,5 +56,81 @@ export class AppComponent {
                         event.previousIndex,
                         event.currentIndex);
     }
+    this.setBoards(this.boards);
   }
+
+  addBoard(inputTitle: HTMLInputElement): void {
+    const title = inputTitle.value.trim();
+    inputTitle.value = '';
+    if (!title.length) {
+      return this.openNotification('Cannot enter empty or whitespace only string!');
+    }
+    const board = { title, items: [] };
+    this.boards.push(board);
+    this.setBoards(this.boards);
+  }
+
+  addTask(indexBoard: number, inputTask: HTMLInputElement): void {
+    const task = inputTask.value.trim();
+    inputTask.value = '';
+    if (!task.length) {
+      return this.openNotification('Cannot enter empty or whitespace only string!');
+    }
+    this.boards[indexBoard].items.push(task);
+    this.setBoards(this.boards);
+  }
+
+  delBoard(indexBoard: number): void {
+    this.openDialog().subscribe(result => {
+      if (result) {
+        this.boards.splice(indexBoard, 1);
+        this.setBoards(this.boards);
+      }
+    });
+  }
+
+  delTask(indexBoard: number, indexTask: number): void {
+    this.openDialog().subscribe(result => {
+      if (result) {
+        this.boards[indexBoard].items.splice(indexTask, 1);
+        this.setBoards(this.boards);
+      }
+    });
+  }
+
+  openDialog(): Observable<boolean> {
+    return this.dialog.open(AppDialogComponent).afterClosed();
+  }
+
+  openNotification(message: string): void {
+    this.snackBar.open(message, null, {
+      duration: 2000
+    });
+  }
+
+  setBoards(boards: Board[]): void {
+    localStorage.setItem('boards', JSON.stringify(boards));
+  }
+
+  getBoards(): Board[] {
+    return JSON.parse(localStorage.getItem('boards'));
+  }
+}
+
+@Component({
+  selector: 'app-root-dialog',
+  template: `
+    <h1 mat-dialog-title>Delete</h1>
+    <div mat-dialog-content>Are you sure to remove this item?</div>
+    <div mat-dialog-actions align="end">
+      <button mat-button mat-dialog-close>Cancel</button>
+      <button mat-button [mat-dialog-close]="true" cdkFocusInitial>Delete</button>
+    </div>
+  `
+})
+export class AppDialogComponent {}
+
+interface Board {
+  title: string;
+  items: string[];
 }
